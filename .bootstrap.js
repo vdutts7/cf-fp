7a3339a41b7dac0720b3cd5660436fe46752c962
Vibesdk <vibesdk-bot@cloudflare.com>
2025-11-10 15:59:03 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add @fingerprintjs/fingerprintjs@^4.4.1"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
